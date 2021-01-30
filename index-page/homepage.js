a70fee65e1a189d5eb573dffdd9675d78f106a5c
mario130 <mario.yonan.work@gmail.com>
2021-01-30 18:10:54 +0200

    started using ajax

--- index-page/homepage.js
+++ index-page/homepage.js
@@ -13,6 +13,25 @@ $.ajax({
   url: "./todos.json",
   data: "data",
   success: function (response) {
-    
+    var lists = $('.card-wrapper .card');
+    for (var i = 0; i < lists.length; i++) {
+      for (var j = 0; j < response[i].length; j++) {
+        $(lists[i]).children('.card-bottom').before('<div class="list-cards"><a href="#" class="list-card">'+response[i][j]+'</a></div>')
+      }
+    }
+    $('.list-cards').draggable({
+      revert: 'invalid'
+    })
   }
 });
+
+$('.card').droppable({
+  accept: '.list-cards',
+})
+
+$('.add-card-btn').click(function(){
+  var newTodo = $(this).parent().prev().val()
+  $(this).parents('.new-task').prev().before('<div class="list-cards"><a href="#" class="list-card">'+newTodo+'</a></div>')
+  $(this).parent().prev().val('')
+})
+
