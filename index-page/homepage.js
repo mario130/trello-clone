c3659a4e4ecf3b4b33e72d761d3439d0b65b5d85
mario130 <mario.yonan.work@gmail.com>
2021-02-01 00:15:14 +0200

    switched to local storage for todos

--- index-page/homepage.js
+++ index-page/homepage.js
@@ -1,37 +1,79 @@
 $(".new-todo-btn").click(function () {
-	// $('#new-todo-show').slideToggle()
-	$(this).parent().next(".new-task").slideToggle();
+  $(this).parent().next(".new-task").slideToggle().children('textarea').focus();
+  
 });
 
 $(".cancel-todo").click(function () {
-	// $('#new-todo-show').slideUp()
 	$(this).parents(".new-task").slideUp();
 });
 
-$.ajax({
-  type: "GET",
-  url: "./todos.json",
-  data: "data",
-  success: function (response) {
-    var lists = $('.card-wrapper .card');
-    for (var i = 0; i < lists.length; i++) {
-      for (var j = 0; j < response[i].length; j++) {
-        $(lists[i]).children('.card-bottom').before('<div class="list-cards"><a href="#" class="list-card">'+response[i][j]+'</a></div>')
-      }
-    }
-    $('.list-cards').draggable({
-      revert: 'invalid'
-    })
-  }
-});
 
-$('.card').droppable({
-  accept: '.list-cards',
-})
+var ls = window.localStorage
+// ls.setItem('listTodo', ['task 1', 'task 2', 'task 3'])
+// ls.setItem('listDoing', ['task 4', 'task 5', 'task 6', 'task 7'])
+// ls.setItem('listDone', ['task 8', 'task 9'])
 
-$('.add-card-btn').click(function(){
+function makeCards(){
+  var listTodos = ls.getItem('listTodo').split(',')
+  var listDoing = ls.getItem('listDoing').split(',')
+  var listDone = ls.getItem('listDone').split(',')
+
+
+
+  listTodos.forEach(function(todo, idx) {
+    $('#listTodos').children('.card-bottom').before('<div class="list-cards"><a href="#" class="list-card" data-idx='+idx+' data-list="listTodo">'+todo+'</a></div>')
+  })
+
+  listDoing.forEach(function(todo, idx) {
+    $('#listDoing').children('.card-bottom').before('<div class="list-cards"><a href="#" class="list-card" data-idx='+idx+' data-list="listDoing">'+todo+'</a></div>')
+  })
+
+  listDone.forEach(function(todo, idx) {
+    $('#listDone').children('.card-bottom').before('<div class="list-cards"><a href="#" class="list-card" data-idx='+idx+' data-list="listDone">'+todo+'</a></div>')
+  })
+  activateCardDeletion()
+}
+makeCards()
+
+$('.add-card-btn').click(function(ev){
   var newTodo = $(this).parent().prev().val()
+
+  var listName = ev.target.dataset.list
+  var listToBeEdited = ls.getItem(`list${listName}`).split(',')
+  listToBeEdited.push(newTodo)
+  ls.setItem(`list${listName}`, listToBeEdited)
+  console.log(ls.getItem('listTodo'));
+  
   $(this).parents('.new-task').prev().before('<div class="list-cards"><a href="#" class="list-card">'+newTodo+'</a></div>')
   $(this).parent().prev().val('')
+  
+  deleteAllCards()
+  makeCards()
+
+  activateCardDeletion()
 })
 
+function activateCardDeletion() {
+  $('.list-card').click(function(ev){
+    // var idx = ev.target.dataset.idx;
+    var listName = ev.target.dataset.list
+    var todoContent = ev.target.innerText
+    var todosBeforeDeletion = ls.getItem(listName).split(',')
+
+    var newTodos = todosBeforeDeletion.filter(function(todo) {
+      if(todo === todoContent) {
+        return false
+      } else {
+        return true
+      }
+    })
+    ls.setItem(listName, newTodos)
+    deleteAllCards()
+    makeCards()
+  })
+}
+activateCardDeletion()
+
+function deleteAllCards() {
+  $('.list-cards').remove()
+}
