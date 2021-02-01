04a90b492702e2877497d7e88fb2ec04ec72cb1c
ahmedElnewery <ahmedm.elnewery@gmail.com>
2021-02-01 22:07:16 +0200

    saving user in localStorage

--- Signup Page/signup.js
+++ Signup Page/signup.js
@@ -1,90 +1,115 @@
+//global variables
+const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const nameRegExp = /^([\w]{3,})+\s+([\w\s]{3,})+$/i;
 
-$(document).ready(function() {
-$(".password-progress::before").css("background", "red")
-    //global variables
-    const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    // validate email and make the input enabled if it is valid
-    $("#email").on("keyup",  function() {
-    if(validateInput($("#email"),emailRegExp)){
-        $("#signup-continue").attr("disabled", false)
-        return true
+$(document).ready(function () {
+  $(".password-progress::before").css("background", "red");
+  // validate email and make the input enabled if it is valid
+  $("#email").on("keyup", function () {
+    if (validateInput($("#email"), emailRegExp)) {
+      $("#signup-continue").attr("disabled", false);
+      return true;
     } else {
-      $("#signup-continue").attr("disabled", true)
-      return false
+      $("#signup-continue").attr("disabled", true);
+      return false;
     }
-})
-/*************************************** */
+  });
+  /*************************************** */
 
-    $("#signup-continue").on("click",function () {
-        $(this).addClass("hidden")
-        $("#signup-submit").removeClass("hidden")
-        $("#fullName").parent().removeClass("hidden")
-        $("#password").parent().removeClass("hidden")
+  $("#signup-continue").on("click", function () {
+    $(this).addClass("hidden");
+    $("#signup-submit").removeClass("hidden");
+    $("#fullName").parent().removeClass("hidden");
+    $("#password").parent().removeClass("hidden");
 
-        $("#password-validation").removeClass("hidden")
+    $("#password-validation").removeClass("hidden");
+  });
+  /*************************************** */
 
-      })
-/*************************************** */
+  $("#signup").on("submit", function (e) {
+    e.preventDefault();
+    const email = $("#email").val();
+    const fullName = $("#fullName").val();
+    const password = $("#password").val();
 
-      $("#signup").on("submit", function(e) {
-        e.preventDefault()
-        const email = $("#email").val()
-        const fullName = $("#fullName").val()
-        const password = $("#password").val()
+    if (validateInput($("#email"), emailRegExp)) {
+      if (validateInput($("#fullName"), nameRegExp)) {
+        if (password.length > 8) {
+          $("#errorMsg").fadeOut("slow").text("");
+          const user = {
+            id: "1",
+            email,
+            fullName,
+            password,
+          };
+          //saving in localstoragw
+            const users = JSON.parse(localStorage.getItem("users")) || [];
+          const newUsers = [...users, user];
+          localStorage.setItem("users", JSON.stringify(newUsers));
+          //initialize values again after login
+          $("#email").val("");
+          $("#fullName").val("");
+          $("#password").val("");
+          //routing to login Page
+          window.location.replace("/index-page/homepage.html")
+        } else {
+     
+          $("#errorMsg")
+            .fadeIn("slow")
+            .text("Password should be Greater than 8 characters");
+        }
+      } else {
+       
+        $("#errorMsg").fadeIn("slow").text("Invalid Name");
+      }
+    } else {
+      
+      $("#errorMsg").text("Invalid Email");
+    }
+  });
+  /*************************************** */
 
-        console.log(checkFullName())
-      })
-      /*************************************** */
-
-      $("#eye-icon").click(function() {
-          
-          if($(this).children().attr("class") ===  "bi bi-eye-slash"){
-              $(this).children().removeClass("bi-eye-slash").addClass("bi-eye")
-              $("#password").attr("type","text")
-          } else if($(this).children().attr("class") ===  "bi bi-eye"){
-            $(this).children().removeClass("bi-eye").addClass("bi-eye-slash")
-            $("#password").attr("type","password")
-
-          }
-      })
-/*********************************************** */
-$("#password").on("keyup",checkPasswordStrength)
-$("#fullName").on("change",checkFullName)
-})
+  $("#eye-icon").click(function () {
+    if ($(this).children().attr("class") === "bi bi-eye-slash") {
+      $(this).children().removeClass("bi-eye-slash").addClass("bi-eye");
+      $("#password").attr("type", "text");
+    } else if ($(this).children().attr("class") === "bi bi-eye") {
+      $(this).children().removeClass("bi-eye").addClass("bi-eye-slash");
+      $("#password").attr("type", "password");
+    }
+  });
+  /*********************************************** */
+  $("#password").on("keyup", checkPasswordStrength);
+  $("#fullName").on("change", checkFullName);
+});
 function checkFullName() {
-    if(! validateInput($("#fullName"),/^([\w]{3,})+\s+([\w\s]{3,})+$/i) ){
-        $("#name-error").text("Invalid full name")
-        return false
-   } else{
-    $("#name-error").text("")
-    return true
+  if (!validateInput($("#fullName"), nameRegExp)) {
+    $("#name-error").text("Invalid full name");
+    return false;
+  } else {
+    $("#name-error").text("");
+    return true;
   }
 }
 /********************************************** */
 
-
-
 //validate any input -- using regexp
-function validateInput(input,RegExp){
-   if( input.val().match(RegExp))
-   return true
-   else return false
+function validateInput(input, RegExp) {
+  if (input.val().match(RegExp)) return true;
+  else return false;
 }
 
-//check for password strength 
+//check for password strength
 function checkPasswordStrength() {
   var password = $("#password").val();
-  var score = 0
+  var score = 0;
   if (password) {
-
-
     if (password.length <= 8) {
-      $(".password-text").text("Password must have at least 8 characters")
-      score = 0
-    }
-    else {
-      $(".password-text").text("")
-      if (password.length > 9) score += 1
+      $(".password-text").text("Password must have at least 8 characters");
+      score = 0;
+    } else {
+      $(".password-text").text("");
+      if (password.length > 9) score += 1;
       if (password.match(/[a-z]/)) score += 1;
       if (password.match(/[A-Z]/)) score += 1;
       if (password.match(/\d+/)) score += 1;
@@ -92,48 +117,59 @@ function checkPasswordStrength() {
     }
     switch (score) {
       case 0:
-        $(".password-text").text("Password must have at least 8 characters")
-        $(".password-progress").removeClass().addClass("password-progress")
+        $(".password-text").text("Password must have at least 8 characters");
+        $(".password-progress").removeClass().addClass("password-progress");
         break;
 
       case 1:
-        $(".password-text").text("Weak")
-        $(".password-progress").removeClass().addClass("password-progress").addClass("weak")
+        $(".password-text").text("Weak");
+        $(".password-progress")
+          .removeClass()
+          .addClass("password-progress")
+          .addClass("weak");
         break;
       case 2:
-        $(".password-text").text("Fair")
-        $(".password-progress").removeClass().addClass("password-progress").addClass("fair")
-
+        $(".password-text").text("Fair");
+        $(".password-progress")
+          .removeClass()
+          .addClass("password-progress")
+          .addClass("fair");
 
         break;
       case 3:
-        $(".password-text").text("Good")
-        $(".password-progress").removeClass().addClass("password-progress").addClass("good")
-
+        $(".password-text").text("Good");
+        $(".password-progress")
+          .removeClass()
+          .addClass("password-progress")
+          .addClass("good");
 
         break;
       case 4:
-        $(".password-text").text("Strong")
-        $(".password-progress").removeClass().addClass("password-progress").addClass("strong")
+        $(".password-text").text("Strong");
+        $(".password-progress")
+          .removeClass()
+          .addClass("password-progress")
+          .addClass("strong");
 
         break;
       case 5:
-        $(".password-text").text("Very Strong")
-        $(".password-progress").removeClass().addClass("password-progress").addClass("verystrong")
+        $(".password-text").text("Very Strong");
+        $(".password-progress")
+          .removeClass()
+          .addClass("password-progress")
+          .addClass("verystrong");
 
         break;
 
       default:
         break;
     }
-
   }
   if (score >= 1) {
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
-
 }
 var docLocation = document.location.search;
 
@@ -142,14 +178,13 @@ if (docLocation) {
   var welcomeEmail = email[1].replace("%40", "@");
   $("#email").val(welcomeEmail);
 }
-const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-var inputEmail = document.getElementById("email");
+
 window.onload = function () {
-  if (validateInput(inputEmail, emailRegExp)) {
-    $("#signup-continue").attr("disabled", false)
-    return true
+  if (validateInput($("#email"), emailRegExp)) {
+    $("#signup-continue").attr("disabled", false);
+    return true;
   } else {
-    $("#signup-continue").attr("disabled", true)
-    return false
+    $("#signup-continue").attr("disabled", true);
+    return false;
   }
-}
+};
