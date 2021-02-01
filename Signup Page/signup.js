6087cf9cb1ee1d6d0b81c0ad41c1ed8ae3f41b99
mario130 <mario.yonan.work@gmail.com>
2021-02-01 23:47:54 +0200

    fixed routing after registering

--- Signup Page/signup.js
+++ Signup Page/signup.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
           $("#fullName").val("");
           $("#password").val("");
           //routing to login Page
-          window.location.replace("/index-page/homepage.html")
+          window.location.replace("../index-page/homepage.html")
         } else {
      
           $("#errorMsg")
