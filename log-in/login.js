a4b144bad57f0a43e7bb046159f0184280cbe8a9
MostafaFathy367 <mostafa.fathy367@gmail.com>
2021-02-03 00:11:47 +0200

    mend the login code

--- log-in/login.js
+++ log-in/login.js
@@ -9,51 +9,40 @@ showpass.click(function () {
   }
 })
 
-var users = localStorage.getItem("users") || [];
+var users = localStorage.getItem("users");
 var newUsers = JSON.parse(users);
 
 var mail = $("#email");
 var send = $("#signup-submit");
 
-var usersMail = [];
-var usersPass = [];
-var UsersId = [];
-var i;
 
-for (i = 0; i < newUsers.length; i++) {
-  usersMail.push(newUsers[i].email);
-}
-for (i = 0; i < newUsers.length; i++) {
-  usersPass.push(newUsers[i].password);
-}
-for (i = 0; i < newUsers.length; i++) {
-  UsersId.push(newUsers[i].id);
-}
 console.log(newUsers);
-var i = 0;
+
+var isRegistered = false;
+
 send.click(function (event) {
   event.preventDefault();
+  isRegistered = false;
   if (localStorage.users) {
+    newUsers.map(function (user) {
+      if (user.email == mail.val()) {
+        if (user.password == pass.val()) {
+          localStorage.setItem("activeUserID", JSON.stringify(user.id))
+          console.log(user.id);
+          isRegistered = true;
+          location.replace("../index-page/homepage.html");
+        }
 
-    if (usersMail.includes(mail.val()) && usersPass.includes(pass.val())) {
-      if (usersMail.indexOf(mail.val()) == usersPass.indexOf(pass.val())) {
-
-        localStorage.setItem("activeUserID", JSON.stringify(UsersId[usersMail.indexOf(mail.val())]))
-        location.replace("../index-page/homepage.html");
-
-      } else {
-        $("#email-error").text("The username or password is invalid");
-        $("#email-error").css("display", "block");
       }
-    } else {
+    })
+
+    if (!isRegistered) {
       $("#email-error").text("The username or password is invalid");
       $("#email-error").css("display", "block");
     }
-
   } else {
     $("#email-error").text("The username or password is invalid");
     $("#email-error").css("display", "block");
   }
-
 })
 
