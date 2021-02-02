19c55e7ae1821d55023d7cdf3c93e8dc127bd688
MostafaFathy367 <mostafa.fathy367@gmail.com>
2021-02-02 21:28:12 +0200

    fixed Login Logc

--- log-in/login.js
+++ log-in/login.js
@@ -8,11 +8,8 @@ showpass.click(function () {
     pass.attr("type", "password")
   }
 })
-var arr = [{ email: "mostafa.fathy367@gmail.com", pass: "Mostafa123" }, { email: "tito_stear@yahoo.com", pass: "titoStear123" }];
-var newArr = JSON.stringify(arr);
-localStorage.setItem("users", newArr);
-var users = localStorage.getItem("users");
 
+var users = localStorage.getItem("users");
 var newUsers = JSON.parse(users);
 
 var mail = $("#email");
@@ -21,31 +18,54 @@ var send = $("#signup-submit");
 var usersMail = [];
 var usersPass = [];
 var i;
-function checkUsers(users) {
-  user = [users.email, users.pass];
-  return user;
 
-}
-console.log(newUsers.map(checkUsers));
 for (i = 0; i < newUsers.length; i++) {
   usersMail.push(newUsers[i].email);
 }
 for (i = 0; i < newUsers.length; i++) {
-  usersPass.push(newUsers[i].pass);
+  usersPass.push(newUsers[i].password);
 }
-
+console.log(newUsers);
 var i = 0;
 send.click(function (event) {
   event.preventDefault();
   if (localStorage.users) {
+
     if (usersMail.includes(mail.val()) && usersPass.includes(pass.val())) {
-      location.replace("../index-page/homepage.html");
+      if (usersMail.indexOf(mail.val()) == usersPass.indexOf(pass.val())) {
+        location.replace("../index-page/homepage.html");
+      } else {
+        $("#email-error").text("The username or password is invalid");
+        $("#email-error").css("display", "block");
+      }
     } else {
       $("#email-error").text("The username or password is invalid");
       $("#email-error").css("display", "block");
     }
+
   } else {
     console.log("user Not Fond");
   }
 
 })
+// send.click(function (event) {
+//   event.preventDefault();
+//   if (localStorage.users) {
+//     for (const iterator of newUsers) {
+//       if (iterator.email == mail.val()) {
+//         if (iterator.password == pass.val()) {
+//           console.log("Welcom")
+//         }
+//       } else {
+//         console.log("worning")
+//       }
+//     }
+//   } else {
+//     console.log("user Not Fond");
+//   }
+
+// })
+
+// for (const iterator of newUsers) {
+//   console.log(iterator)
+// }
