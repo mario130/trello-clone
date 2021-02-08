0815a2f798186b74cf046f5b1daae168cfb4d53f
mario130 <mario.yonan.work@gmail.com>
2021-02-09 01:07:10 +0200

    formatting and deleting - 3 + fixed welcome page hrefs

--- index-page/modules/login-verify.js
+++ index-page/modules/login-verify.js
@@ -1,18 +1,16 @@
-import {getData} from './helper-functions.js'
+import { getData } from "./helper-functions.js";
 
 // detect if user passed login
-if (!(getData('activeUserID'))){
-  document.location.href = "/log-in/login.html";
+if (!getData("activeUserID")) {
+	document.location.href = "/log-in/login.html";
 }
-  
-  // logout logic
-  function logOut() {
-    localStorage.removeItem("activeUserID");
-    localStorage.removeItem("activeTeamIndex");
-    location.assign("../log-in/login.html");
-  }
-  $('#log-out').click(function(){
-    logOut()
-  })
 
-  
+// logout logic
+function logOut() {
+	localStorage.removeItem("activeUserID");
+	localStorage.removeItem("activeTeamIndex");
+	location.assign("../../welcome-page/welcome.html");
+}
+$("#log-out").click(function () {
+	logOut();
+});
