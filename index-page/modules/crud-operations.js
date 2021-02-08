78c7e467207e4c6ec85f02e77281a5fd60ab7d42
mario130 <mario.yonan.work@gmail.com>
2021-02-08 19:06:52 +0200

    fix add list bugs

--- index-page/modules/crud-operations.js
+++ index-page/modules/crud-operations.js
@@ -23,6 +23,35 @@ for (let i = 0; i < teams.length; i++){     //loop on teams (used normal loop to
 
 //adding another list
 $("#addlistBtn").click(function () {
+
+
+
+
+  let activeTeamIndex;
+  let teams = getData('teams');
+  let boards;
+  let currentActiveBoardIdx = getData('currentActiveBoardIdx');
+
+
+
+  for (let i = 0; i < teams.length; i++){     //loop on teams (used normal loop to get team index from i)
+    for (let member of teams[i].teamMembers){ //loop on team members
+      for (let user of getData('users')){     //loop on users to compare
+        
+        if (member === user.userName){ // match the team which has this user's id to get the team's boards
+        boards = teams[i].boards
+        const activeTeamId = teams[i].id
+        activeTeamIndex = i
+        }
+      }
+    }
+  }
+  
+  
+  
+  
+  
+  
   //create list object and push
   var listTitle = $(this).parent().prev().val();
   var isAlready=false
@@ -88,8 +117,34 @@ export function deleteList(that){
 
 //click on add-card-btn and adding todo in
 $("#listItems").on("click", ".add-card-btn", function (ev) {
+
+
+
+  let activeTeamIndex;
+  let teams = getData('teams');
+  let boards;
+  let currentActiveBoardIdx = getData('currentActiveBoardIdx');
+
+
+
+  for (let i = 0; i < teams.length; i++){     //loop on teams (used normal loop to get team index from i)
+    for (let member of teams[i].teamMembers){ //loop on team members
+      for (let user of getData('users')){     //loop on users to compare
+        
+        if (member === user.userName){ // match the team which has this user's id to get the team's boards
+        boards = teams[i].boards
+        const activeTeamId = teams[i].id
+        activeTeamIndex = i
+        }
+      }
+    }
+  }
+  
+  
+  
+  
+  
   teams = getData('teams');
-  var currentActiveBoardIdx = getData('currentActiveBoardIdx')
   var todoName = $(this).parent().prev().val();
   if (todoName.trim() === "") return;
 
